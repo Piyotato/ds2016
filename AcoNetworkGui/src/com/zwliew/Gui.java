4377351d9e4f5c9e85da626f24d0e75707872e98
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-04-02 18:16:58 +0800

    AcoNetworkGui: Slight clean up
     * Remove redundant comments
     * Streamline the component initialization process

--- AcoNetworkGui/src/com/zwliew/Gui.java
+++ AcoNetworkGui/src/com/zwliew/Gui.java
@@ -25,33 +25,68 @@ public class Gui extends JFrame {
     }
 
     private void initComponents() {
-        // Exit program when GUI is closed
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        // Initialize the main panel
-        JPanel container = new JPanel();
-        container.setLayout(new BoxLayout(container, BoxLayout.X_AXIS));
-
-        // Initialize the 2 sub-panels
-        JPanel networkPanel = new JPanel();
-        JPanel configPanel = new JPanel();
-
-        // Allocate a width ratio of about 3 to 2
-        networkPanel.setPreferredSize(new Dimension(538, 504));
-        configPanel.setPreferredSize(new Dimension(358, 504));
-
-        // TODO: Populate the sub-panels
-        networkPanel.add(new JLabel("NETWORK PANEL"));
-        configPanel.add(new JLabel("CONFIG PANEL"));
-
-        // Populate the content pane with the panels
-        container.add(networkPanel);
-        container.add(configPanel);
-        setContentPane(container);
-
-        // Set title and frame size
         setTitle(FRAME_TITLE);
         setSize(896, 504); // Completely arbitrary 16:9 ratio divisible by 8
 
+        JPanel networkPanel = initNetworkPanel();
+        JPanel configPanel = initConfigPanel();
+        JPanel container = initContainer(networkPanel, configPanel);
+        setContentPane(container);
+    }
+
+    /**
+     * Initializes a new container for containing all the UI
+     * elements in the program
+     *
+     * @param panels The JPanels that hold the smaller UI elemtnts
+     * @return the newly initialized container
+     */
+    private JPanel initContainer(JPanel... panels) {
+        JPanel container = new JPanel();
+        container.setLayout(new BoxLayout(container, BoxLayout.X_AXIS));
+
+        for (JPanel panel : panels) {
+            container.add(panel);
+        }
+
+        return container;
+    }
+
+    /**
+     * Initializes a new network panel for displaying
+     * the router nodes being simulated
+     *
+     * @return the newly initialized network panel
+     */
+    private JPanel initNetworkPanel() {
+        JPanel panel = new JPanel();
+
+        panel.setPreferredSize(new Dimension(538, 504));
+
+        /*
+         * TODO: Initialize the graph
+         */
+
+        return panel;
+    }
+
+    /**
+     * Initializes a new config panel for displaying
+     * the config options being controlled
+     *
+     * @return the newly initialized config panel
+     */
+    private JPanel initConfigPanel() {
+        JPanel panel = new JPanel();
+
+        panel.setPreferredSize(new Dimension(358, 504));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        /*
+         * TODO: Add in all the config options
+         */
+
+        return panel;
     }
 }
