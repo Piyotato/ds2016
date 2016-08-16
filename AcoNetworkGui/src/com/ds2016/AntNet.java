Piyotato/ds2016

commit c1b65b3645f434d98342d81a1c97d30dc8a940dc

More Misc Changes hasInit -> didInit Properly document @throws Node_EACO now unionSets addition updates rather than rebuilding Node_EACO now caches numViableNeighbours for each destination

AcoNetworkGui/src/com/ds2016/AntNet.java
@@ -17,7 +17,7 @@ class AntNet implements AlgorithmBase {
     private final ArrayList<Node_AntNet> nodes = new ArrayList<>();
     private int source, destination;
     private int success, failure, currentTime, packetCnt, numAntsGen;
-    private boolean hasInit;
+    private boolean didInit;
 
     /**
      * Initialize EACO
@@ -44,7 +44,7 @@ class AntNet implements AlgorithmBase {
         for (Node_AntNet node: nodes) {
             node.init();
         }
-        hasInit = true;
+        didInit = true;
     }
 
     /**
@@ -86,7 +86,7 @@ class AntNet implements AlgorithmBase {
      * Toggle state of a node
      *
      * @param ID Node ID
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException if ID is out of bounds
      */
     public void toggleNode(int ID) throws IllegalArgumentException {
         if (ID == source || ID == destination) {
@@ -104,7 +104,7 @@ class AntNet implements AlgorithmBase {
                 edge.ants.clear();
             }
         }
-        if (hasInit)
+        if (didInit)
             for (Edge_ACO edge: adjMat.get(ID).values()) {
                 nodes.get(edge.destination).toggleNode(ID);
             }
@@ -116,7 +116,7 @@ class AntNet implements AlgorithmBase {
      * @param node1 First node
      * @param node2 Second node
      * @param cost Time taken
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException if ID is out of bounds
      */
     public void addEdge(int node1, int node2, int cost) throws IllegalArgumentException {
         if (node1 >= nodes.size() || node2 >= nodes.size()) {
@@ -128,7 +128,7 @@ class AntNet implements AlgorithmBase {
         edgeList.add(backward);
         adjMat.put(node1, node2, forward);
         adjMat.put(node2, node1, backward);
-        if (hasInit) {
+        if (didInit) {
             nodes.get(node1).addEdge(node2);
             nodes.get(node2).addEdge(node1);
         }
@@ -152,7 +152,7 @@ class AntNet implements AlgorithmBase {
             backward.packets.clear();
             backward.ants.clear();
         }
-        if (hasInit) {
+        if (didInit) {
             nodes.get(forward.source).toggleEdge(forward.destination);
             nodes.get(forward.destination).toggleEdge(forward.source);
         }

AcoNetworkGui/src/com/ds2016/EACO.java
@@ -17,7 +17,7 @@ class EACO implements AlgorithmBase {
     private final ArrayList<Node_EACO> nodes = new ArrayList<>();
     private int source, destination;
     private int success, failure, currentTime, packetCnt, numAntsGen;
-    private boolean hasInit;
+    private boolean didInit;
 
     /**
      * Initialize EACO
@@ -44,7 +44,7 @@ class EACO implements AlgorithmBase {
         for (Node_EACO node : nodes) {
             node.init();
         }
-        hasInit = true;
+        didInit = true;
     }
 
     /**
@@ -86,7 +86,7 @@ class EACO implements AlgorithmBase {
      * Toggle state of a node
      *
      * @param ID Node ID
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException if ID is out of bounds
      */
     public void toggleNode(int ID) throws IllegalArgumentException {
         if (ID == source || ID == destination) {
@@ -104,9 +104,9 @@ class EACO implements AlgorithmBase {
                 edge.ants.clear();
             }
         }
-        if (hasInit)
+        if (didInit)
             for (Node_EACO _node : nodes) {
-                _node.rebuild();
+                _node.toggleNode(ID);
             }
     }
 
@@ -116,7 +116,7 @@ class EACO implements AlgorithmBase {
      * @param node1 First node
      * @param node2 Second node
      * @param cost  Time taken
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException if ID is out of bounds
      */
     public void addEdge(int node1, int node2, int cost) throws IllegalArgumentException {
         if (node1 >= nodes.size() || node2 >= nodes.size()) {
@@ -128,9 +128,9 @@ class EACO implements AlgorithmBase {
         edgeList.add(backward);
         adjMat.put(node1, node2, forward);
         adjMat.put(node2, node1, backward);
-        if (hasInit)
+        if (didInit)
             for (Node_EACO node : nodes) {
-                node.rebuild();
+                node.addEdge(node1, node2);
             }
     }
 
@@ -152,9 +152,9 @@ class EACO implements AlgorithmBase {
             backward.packets.clear();
             backward.ants.clear();
         }
-        if (hasInit)
+        if (didInit)
             for (Node_EACO node : nodes) {
-                node.rebuild();
+                node.toggleEdge(ID);
             }
     }
 

AcoNetworkGui/src/com/ds2016/Node_EACO.java
@@ -16,6 +16,7 @@ class Node_EACO {
     final HashMap2D<Integer, Integer, Double> pheromone = new HashMap2D<>(); // Destination, Node
     final ArrayDeque<Ant> fastQ = new ArrayDeque<>();
     final ArrayDeque<Packet> slowQ = new ArrayDeque<>();
+    private ArrayList<Integer> numViableNeighbours = new ArrayList<>();
     private final double alpha;
     private final ArrayList<Node_EACO> nodes;
     private final ArrayList<Edge_ACO> edgeList;
@@ -59,6 +60,7 @@ class Node_EACO {
                 pheromone.put(a, edge.destination, 1. / numNeighbours);
                 routing.put(a, edge.destination, 1. / numNeighbours);
             }
+            numViableNeighbours.add(numNeighbours);
         }
     }
 
@@ -72,6 +74,59 @@ class Node_EACO {
             if (edge.isOffline) continue;
             DSU.unionSet(edge.source, edge.destination);
         }
+    }
+
+    /**
+     * React to toggling of node
+     *
+     * @param ID Node ID
+     */
+    void toggleNode(int ID) {
+        if (nodes.get(nodeID).isOffline) {
+            initDSU();
+        } else {
+            // Merge edges
+            for (Edge_ACO edge : adjMat.get(ID).values()) {
+                if (!edge.isOffline) {
+                    // Don't merge self
+                    if (edge.source == nodeID || edge.destination == nodeID) continue;
+                    DSU.unionSet(edge.source, edge.destination);
+                }
+            }
+        }
+        update();
+    }
+
+    /**
+     * React to addition of an edge
+     *
+     * @param node1 First node
+     * @param node2 Second node
+     */
+    void addEdge(int node1, int node2) {
+        // Don't merge self
+        if (node1 == nodeID || node2 == nodeID) return;
+        DSU.unionSet(node1, node2);
+        update();
+    }
+
+    /**
+     * React to toggling of an edge
+     *
+     * @param ID Edge ID
+     */
+    void toggleEdge(int ID) {
+        // If self is involved, it doesn't affect viability
+        int src = edgeList.get(ID).source;
+        int dst = edgeList.get(ID).destination;
+        if (src == nodeID || dst == nodeID)
+            return;
+        if (edgeList.get(ID).isOffline) {
+            initDSU();
+        } else {
+            DSU.unionSet(src, dst);
+        }
+        update();
     }
 
     /**
@@ -106,13 +161,15 @@ class Node_EACO {
     }
 
     /**
-     * Rebuild DSU
      * Update heuristic
      */
-    void rebuild() {
+    private void update() {
+        // Resize just in case
+        while (numViableNeighbours.size() < nodes.size()) {
+            numViableNeighbours.add(0);
+        }
         ArrayList<Integer> neighbours = new ArrayList<>();
         ArrayList<Integer> destinations = new ArrayList<>();
-        initDSU();
         for (Edge_ACO edge : adjMat.get(nodeID).values()) {
             neighbours.add(edge.destination);
         }
@@ -127,11 +184,13 @@ class Node_EACO {
                     if (DSU.sameSet(neighbour, dest) &&
                             !adjMat.get(nodeID, neighbour).isOffline &&
                             !nodes.get(neighbour).isOffline) // Now viable
+                        numViableNeighbours.set(dest, numViableNeighbours.get(dest) + 1);
                         addHeuristic(neighbour, dest);
                 } else { // Previously viable
                     if (!DSU.sameSet(neighbour, dest) ||
                             adjMat.get(nodeID, neighbour).isOffline ||
                             nodes.get(neighbour).isOffline) // Now not viable
+                        numViableNeighbours.set(dest, numViableNeighbours.get(dest) - 1);
                         removeHeuristic(neighbour, dest);
                 }
             }
@@ -146,7 +205,7 @@ class Node_EACO {
      * @param neighbour   ID of neighbour
      * @param destination ID of destination
      * @param change      Pheromone change
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException if pheromone lies out of range [0, 1]
      */
     void updateHeuristic(int neighbour, int destination, double change) throws IllegalArgumentException {
         Double old = pheromone.get(destination, neighbour);
@@ -206,12 +265,7 @@ class Node_EACO {
      * @param destination Destination ID
      */
     private void addHeuristic(int neighbour, int destination) {
-        int numNeighbours = 0;
-        for (Edge_ACO edge : adjMat.get(nodeID).values()) {
-            if (edge.isOffline || nodes.get(edge.destination).isOffline) continue;
-            if (DSU.sameSet(edge.destination, destination)) ++numNeighbours;
-        }
-        updateHeuristic(neighbour, destination, 1. / numNeighbours);
+        updateHeuristic(neighbour, destination, 1. / numViableNeighbours.get(destination));
     }
 
     /**

AcoNetworkGui/src/com/ds2016/OSPF.java
@@ -80,7 +80,7 @@ class OSPF implements AlgorithmBase {
      * Toggle state of anode
      *
      * @param ID Node ID
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException if ID is out of bounds
      */
     public void toggleNode(int ID) throws IllegalArgumentException {
         if (ID == source || ID == destination) {

AcoNetworkGui/src/com/ds2016/NewGui.java
@@ -231,7 +231,7 @@ public class NewGui {
      * Toggle state of a node
      *
      * @param ID Node ID
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException if ID is out of bounds
      */
     void toggleNode(int ID) throws IllegalArgumentException {
         if (ID == sParams.getSource() || ID == sParams.getDestination()) {
@@ -257,7 +257,7 @@ public class NewGui {
      * @param node1 First node
      * @param node2 Second node
      * @param cost  Time taken
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException if ID is out of bounds
      */
     void addEdge(int node1, int node2, int cost) throws IllegalArgumentException {
         // We can't add edges between non-existent nodes

AcoNetworkGui/src/com/ds2016/Node_AntNet.java
@@ -150,7 +150,7 @@ class Node_AntNet {
      * @param neighbour ID of neighbour
      * @param destination ID of destination
      * @param change Pheromone change
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException if pheromone lies out of range [0, 1]
      */
     void updateHeuristic(int neighbour, int destination, double change) throws IllegalArgumentException {
         Double old = pheromone.get(destination, neighbour);

commit 03b3c72583047d8a4b1c1ae53e5f320975f312e4

Added traffic load parameter Fixed error in Node_EACO.update() introduced in c1b65b3

AcoNetworkGui/src/com/ds2016/AntNet.java
@@ -11,7 +11,7 @@ import java.util.Random;
 class AntNet implements AlgorithmBase {
 
     private final double alpha, interval;
-    private final int TTL;
+    private final int TTL, traffic;
     private final ArrayList<Edge_ACO> edgeList = new ArrayList<>();
     private final HashMap2D<Integer, Integer, Edge_ACO> adjMat = new HashMap2D<>();
     private final ArrayList<Node_AntNet> nodes = new ArrayList<>();
@@ -23,11 +23,13 @@ class AntNet implements AlgorithmBase {
      * Initialize EACO
      *
      * @param _alpha    Weightage of pheromone
+     * @param _traffic  Packets per tick
      * @param _TTL      Time To Live of packets
      * @param _interval Interval of Ant Generation
      */
-    public AntNet(double _alpha, int _TTL, double _interval) {
+    public AntNet(double _alpha, int _traffic, int _TTL, double _interval) {
         alpha = _alpha;
+        traffic = _traffic;
         TTL = _TTL;
         interval = _interval;
     }
@@ -277,8 +279,8 @@ class AntNet implements AlgorithmBase {
         numAntsGen = curNumAnts;
         Node_AntNet src = nodes.get(source);
         // Send packets from source node
-        packetCnt += Math.max(src.speed - src.slowQ.size(), 0);
-        while (src.slowQ.size() < src.speed) {
+        packetCnt += traffic;
+        for (int cnt = 0; cnt < traffic; ++cnt) {
             src.slowQ.add(new Packet(source, destination, TTL, currentTime));
         }
     }

AcoNetworkGui/src/com/ds2016/EACO.java
@@ -11,7 +11,7 @@ import java.util.Random;
 class EACO implements AlgorithmBase {
 
     private final double alpha, interval;
-    private final int TTL;
+    private final int TTL, traffic;
     private final ArrayList<Edge_ACO> edgeList = new ArrayList<>();
     private final HashMap2D<Integer, Integer, Edge_ACO> adjMat = new HashMap2D<>();
     private final ArrayList<Node_EACO> nodes = new ArrayList<>();
@@ -23,11 +23,13 @@ class EACO implements AlgorithmBase {
      * Initialize EACO
      *
      * @param _alpha    Weightage of pheromone
+     * @param _traffic  Packets per tick
      * @param _TTL      Time To Live of packets
      * @param _interval Interval of Ant Generation
      */
-    public EACO(double _alpha, int _TTL, double _interval) {
+    public EACO(double _alpha, int _traffic, int _TTL, double _interval) {
         alpha = _alpha;
+        traffic = _traffic;
         TTL = _TTL;
         interval = _interval;
     }
@@ -277,8 +279,8 @@ class EACO implements AlgorithmBase {
         numAntsGen = curNumPackets;
         Node_EACO src = nodes.get(source);
         // Send packets from source node
-        packetCnt += Math.max(src.speed - src.slowQ.size(), 0);
-        while (src.slowQ.size() < src.speed) {
+        packetCnt += traffic;
+        for (int cnt = 0; cnt < traffic; ++cnt) {
             src.slowQ.add(new Packet(source, destination, TTL, currentTime));
         }
     }

AcoNetworkGui/src/com/ds2016/Node_EACO.java
@@ -185,15 +185,17 @@ class Node_EACO {
                 if (prev == null) { // Previously not viable
                     if (DSU.sameSet(neighbour, dest) &&
                             !adjMat.get(nodeID, neighbour).isOffline &&
-                            !nodes.get(neighbour).isOffline) // Now viable
+                            !nodes.get(neighbour).isOffline) { // Now viable
                         numViableNeighbours.set(dest, numViableNeighbours.get(dest) + 1);
-                    addHeuristic(neighbour, dest);
+                        addHeuristic(neighbour, dest);
+                    }
                 } else { // Previously viable
                     if (!DSU.sameSet(neighbour, dest) ||
                             adjMat.get(nodeID, neighbour).isOffline ||
-                            nodes.get(neighbour).isOffline) // Now not viable
+                            nodes.get(neighbour).isOffline) { // Now not viable
                         numViableNeighbours.set(dest, numViableNeighbours.get(dest) - 1);
-                    removeHeuristic(neighbour, dest);
+                        removeHeuristic(neighbour, dest);
+                    }
                 }
             }
         }

AcoNetworkGui/src/com/ds2016/OSPF.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
  */
 class OSPF implements AlgorithmBase {
 
-    private final int TTL;
+    private final int TTL, traffic;
     private final ArrayList<Node_OSPF> nodes = new ArrayList<>();
     private final ArrayList<Edge> edgeList = new ArrayList<>();
     private final HashMap2D<Integer, Integer, Edge> adjMat = new HashMap2D<>();
@@ -21,9 +21,11 @@ class OSPF implements AlgorithmBase {
      * Initialize OSPF
      *
      * @param _TTL Time To Live of packets
+     * @param _traffic  Packets per tick
      */
-    public OSPF(int _TTL) {
+    public OSPF(int _TTL, int _traffic) {
         TTL = _TTL;
+        traffic = _traffic;
     }
 
     /**
@@ -195,8 +197,8 @@ class OSPF implements AlgorithmBase {
      */
     private void generatePackets() {
         Node_OSPF src = nodes.get(source);
-        packetCnt += Math.max(src.speed - src.Q.size(), 0);
-        while (src.Q.size() < src.speed) {
+        packetCnt += traffic;
+        for (int cnt = 0; cnt < traffic; ++cnt) {
             src.Q.add(new Packet(source, destination, TTL, currentTime));
         }
     }