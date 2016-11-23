2632ad026b7df7628f9c8ccf0a8e2e0585e464f5
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-23 15:41:01 +0800

    Comment out currently unused graphs
    These don't compile with the new method arguments.
    We don't use them much anyways, so just comment them out.

--- AcoNetworkGui/src/com/ds2016/Link.java
+++ AcoNetworkGui/src/com/ds2016/Link.java
@@ -96,113 +96,112 @@ class Link {
         sDataChart.resetCharts();
     }
 
-    /**
-     * OSPF chokes; others ok
-     */
-    static void buildDiamond() {
-        // Add 4 nodes
-        addNode(1000);
-        addNode(600);
-        addNode(800);
-        addNode(600);
+    /*
+        // OSPF chokes; others ok
+        static void buildDiamond() {
+            // Add 4 nodes
+            addNode(1000);
+            addNode(600);
+            addNode(800);
+            addNode(600);
 
-        addEdge(0, 1, 4);
-        addEdge(1, 2, 6);
-        addEdge(2, 3, 6);
-        addEdge(3, 0, 5);
-    }
+            addEdge(0, 1, 4);
+            addEdge(1, 2, 6);
+            addEdge(2, 3, 6);
+            addEdge(3, 0, 5);
+        }
 
-    static void buildDoubleDiamond() {
-        // Add 7 nodes
-        addNode(1000);
-        addNode(600);
-        addNode(600);
-        addNode(1000);
-        addNode(600);
-        addNode(600);
-        addNode(800);
+        static void buildDoubleDiamond() {
+            // Add 7 nodes
+            addNode(1000);
+            addNode(600);
+            addNode(600);
+            addNode(1000);
+            addNode(600);
+            addNode(600);
+            addNode(800);
 
-        addEdge(0, 1, 4);
-        addEdge(0, 2, 6);
-        addEdge(1, 3, 4);
-        addEdge(2, 3, 4);
-        addEdge(3, 4, 4);
-        addEdge(3, 5, 4);
-        addEdge(4, 6, 4);
-        addEdge(5, 6, 4);
-    }
+            addEdge(0, 1, 4);
+            addEdge(0, 2, 6);
+            addEdge(1, 3, 4);
+            addEdge(2, 3, 4);
+            addEdge(3, 4, 4);
+            addEdge(3, 5, 4);
+            addEdge(4, 6, 4);
+            addEdge(5, 6, 4);
+        }
 
-    static void buildBananaTree() {
-        // Add 31 nodes
-        addNode(1000);
-        addNode(800);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(800);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
-        addNode(600);
+        static void buildBananaTree() {
+            // Add 31 nodes
+            addNode(1000);
+            addNode(800);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(800);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
+            addNode(600);
 
-        addEdge(0, 1, 4);
-        addEdge(0, 2, 4);
-        addEdge(0, 3, 4);
-        addEdge(0, 4, 4);
-        addEdge(0, 5, 4);
+            addEdge(0, 1, 4);
+            addEdge(0, 2, 4);
+            addEdge(0, 3, 4);
+            addEdge(0, 4, 4);
+            addEdge(0, 5, 4);
 
-        addEdge(1, 6, 4);
-        addEdge(6, 7, 4);
-        addEdge(6, 8, 4);
-        addEdge(6, 9, 4);
-        addEdge(6, 10, 4);
+            addEdge(1, 6, 4);
+            addEdge(6, 7, 4);
+            addEdge(6, 8, 4);
+            addEdge(6, 9, 4);
+            addEdge(6, 10, 4);
 
-        addEdge(2, 11, 4);
-        addEdge(11, 12, 4);
-        addEdge(11, 13, 4);
-        addEdge(11, 14, 4);
-        addEdge(11, 15, 4);
+            addEdge(2, 11, 4);
+            addEdge(11, 12, 4);
+            addEdge(11, 13, 4);
+            addEdge(11, 14, 4);
+            addEdge(11, 15, 4);
 
-        addEdge(3, 16, 4);
-        addEdge(16, 17, 4);
-        addEdge(16, 18, 4);
-        addEdge(16, 19, 4);
-        addEdge(16, 20, 4);
+            addEdge(3, 16, 4);
+            addEdge(16, 17, 4);
+            addEdge(16, 18, 4);
+            addEdge(16, 19, 4);
+            addEdge(16, 20, 4);
 
-        addEdge(4, 21, 4);
-        addEdge(21, 22, 4);
-        addEdge(21, 23, 4);
-        addEdge(21, 24, 4);
-        addEdge(21, 25, 4);
+            addEdge(4, 21, 4);
+            addEdge(21, 22, 4);
+            addEdge(21, 23, 4);
+            addEdge(21, 24, 4);
+            addEdge(21, 25, 4);
 
-        addEdge(5, 26, 4);
-        addEdge(26, 27, 4);
-        addEdge(26, 28, 4);
-        addEdge(26, 29, 4);
-        addEdge(26, 30, 4);
-    }
-
+            addEdge(5, 26, 4);
+            addEdge(26, 27, 4);
+            addEdge(26, 28, 4);
+            addEdge(26, 29, 4);
+            addEdge(26, 30, 4);
+        }
+    */
     static void buildNsfNet() {
         addNode();
         addNode();
