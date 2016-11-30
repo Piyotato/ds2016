b42293279646cc95fa7a756229e999e0989d9636
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-30 13:27:34 +0800

    Disable debug by default
    Accidentally kept it enabled previously. This should never be enabled by default.

--- AcoNetworkGui/src/com/ds2016/Main.java
+++ AcoNetworkGui/src/com/ds2016/Main.java
@@ -4,7 +4,7 @@ package com.ds2016;
  * Created by zwliew on 13/6/16.
  */
 public class Main {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     public static void main(String[] args) {
         final Link link = new Link();
