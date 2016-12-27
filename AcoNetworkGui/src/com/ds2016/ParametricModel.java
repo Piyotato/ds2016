2b2d214f6d6c9b33c90fa4d30b453472f5b06dba
Damian Ho <damian_ho_xu_yang@yahoo.com>
2016-12-28 00:31:09 +0800

    More fixes to ParametricModel

--- AcoNetworkGui/src/com/ds2016/ParametricModel.java
+++ AcoNetworkGui/src/com/ds2016/ParametricModel.java
@@ -1,7 +1,5 @@
 package com.ds2016;
 
-import java.util.ArrayDeque;
-
 /**
  * Created by damian on 30/11/16.
  */
@@ -9,19 +7,18 @@ public class ParametricModel {
 
     // Empirical Values
     // γ = Confidence Coefficient
-    private final static double c1 = 0.7, c2 = 0.3, γ = 0.7, z = 1. / Math.sqrt(1 - γ);
+    private final static double c1 = 0.63, c2 = 0.27, γ = 0.95, z = 1. / Math.sqrt(1 - γ);
     // η = Effective Samples
-    // a and c have no empirical values...
-    private final static double η = 0.1, a = 1, c = 1;
+    // a has no empirical value
+    private final static double η = 0.005, a = 10, c = 0.3;
     // w = Size of window (Dependant on value of η)
-    private final static int w = (int) (5 * (c / η));
-    private final ArrayDeque<Double> window = new ArrayDeque<>();
-    private final ArrayDeque<Double> minDeque = new ArrayDeque<>();
+    private final static int w = (int) (5 * (c / η) + 0.5);
     private final int N;
     // μ = Sample Mean
     // σ2 = Sample Variance
     // W = Sample Minimum
-    private double μ, σ2, W;
+    private double μ, σ2, W = Double.MAX_VALUE;
+    private int windowCount;
 
     /**
      * Build Parametric Model
@@ -30,9 +27,6 @@ public class ParametricModel {
      */
     ParametricModel(int _N) {
         N = _N;
-        System.out.println("Initialising...");
-        System.out.println("N is: " + N);
-        System.out.println("w is: " + w);
     }
 
     /**
@@ -44,8 +38,8 @@ public class ParametricModel {
      * @throws IllegalArgumentException if X is out of the range (0, 1]
      */
     private double squash(double X) throws IllegalArgumentException {
-        if (X > 1) throw new IllegalArgumentException("Got: " + X);
-        return 1. / (1 + Math.exp(a / (X * N)));
+        if (X > 1) throw new IllegalArgumentException();
+        return (1 + Math.exp(a / N)) / (1 + Math.exp(a / (X * N)));
     }
 
     /**
@@ -55,36 +49,29 @@ public class ParametricModel {
      * @return Reinforcement Value
      */
     double getReinforcement(double T) {
-        while (!minDeque.isEmpty() && minDeque.peekLast() > T) {
-            //System.out.println("Deleting: " + minDeque.peekLast());
-            minDeque.pollLast();
-        }
-        window.offerLast(T);
-        minDeque.offerLast(T);
-        if (window.size() > w) {
-            if (window.peekFirst().equals(minDeque.peekFirst())) {
-                minDeque.pollFirst();
-            }
-            window.pollFirst();
-        }
         // Update Variables
-        μ = μ + η * (T - μ);
-        σ2 = σ2 + η * ((T - μ) * (T - μ) - σ2);
-        W = minDeque.peekFirst();
-        //System.out.println("New value of μ: " + μ);
-        //System.out.println("New value of σ2: " + σ2);
-        //System.out.println("New value of W: " + W);
+        if (μ == 0) {
+            μ = T;
+        } else {
+            μ = μ + η * (T - μ);
+            σ2 = σ2 + η * ((T - μ) * (T - μ) - σ2);
+        }
+        if (++windowCount > w) {
+            windowCount = 1;
+            W = μ;
+        }
+        if (T < W) {
+            W = T;
+        }
         // Calculate reinforcement
         double I_inf = W;
-        double I_sup = μ + z * (Math.sqrt(σ2) / Math.sqrt(w));
-        //System.out.println("I_inf: " + I_inf);
-        //System.out.println("I_sup: " + I_sup);
-        //System.out.println(μ + " + " + z + " * " + (Math.sqrt(σ2) / Math.sqrt(w)));
-        double r = c1 * (W / T) + c2 * ((I_sup - I_inf) / ((I_sup - I_inf) + (T - I_inf)));
-        //System.out.println("Initial value of r: " + r);
-        //System.out.println(c1 * (W / T) + " + " + c2 * ((I_sup - I_inf) / ((I_sup - I_inf) + (T - I_inf))));
-        //System.out.println("squash(r): " + squash(r));
-        //System.out.println("squash(1): " + squash(1));
+        double I_sup = μ + z * Math.sqrt(σ2 / w);
+        double r;
+        if ((I_sup - I_inf) + (T - I_inf) == 0) {
+            r = (c1 + c2) * (W / T);
+        } else {
+            r = c1 * (W / T) + c2 * ((I_sup - I_inf) / ((I_sup - I_inf) + (T - I_inf)));
+        }
         return squash(r) / squash(1);
     }
 }
